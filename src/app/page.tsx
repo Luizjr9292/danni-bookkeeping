ccd0d1e844210721e167124a330413501ad85dd9
Luizjr9292 <luizjr92@gmail.com>
2026-03-29 16:28:25 +1300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { TrustBar } from "@/components/sections/TrustBar";
 import { Services } from "@/components/sections/Services";
 import { VirtualAdvantage } from "@/components/sections/VirtualAdvantage";
 import { Process } from "@/components/sections/Process";
-import { Pricing } from "@/components/sections/Pricing";
 import { About } from "@/components/sections/About";
+import { Pricing } from "@/components/sections/Pricing";
 import { Contact } from "@/components/sections/Contact";
 
 export default function Home() {
@@ -15,8 +15,8 @@ export default function Home() {
       <Services />
       <VirtualAdvantage />
       <Process />
-      <Pricing />
       <About />
+      <Pricing />
       <Contact />
     </div>
   );
